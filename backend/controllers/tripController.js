b7b741bab21cdf09d78a55f6fab64530052f18c6
Abhishek Kapoor <kapoor98.ak@gmail.com>
2024-07-16 10:55:58 -0300

    Create Trip, Filter Trip is done

--- backend/controllers/tripController.js
+++ backend/controllers/tripController.js
@@ -1,14 +1,30 @@
 const tripService = require('../services/tripService');
 
-const createTrip = async (req, res) => {
-    // try {
-    //   const user = await userService.createUser(req.body);
-    //   res.status(201).json(user);
-    // } catch (error) {
-    //   res.status(500).json({ message: error.message });
-    // }
+exports.createTrip = async (req, res) => {
+    const { title, description, source, destination, startDate, endDate, price, agentId, capacity } = req.body;
+
+    if(!title || !description || !source || !destination || !startDate || !endDate || !price || !agentId || !capacity){
+      return res.status(400).json({msg: 'Please enter all the details!!'});
+    }
+    try {
+      const tripData = { title, description, source, destination, startDate, endDate, price, agentId, capacity };
+      const newTrip = await tripService.createTrip(tripData);
+      res.status(201).json(newTrip);
+    } catch (error) {
+      console.error('Error while creating trip:', error);
+      res.status(500).json({ msg: 'Server error' });
+    }
   };
-  
-  module.exports = {
-    createTrip, 
-  };
+
+
+exports.filterTrips = async (req, res) => {
+  const { source, destination, startDate, endDate } = req.body;
+
+  try {
+    const trips = await tripService.filterTrips({ source, destination, startDate, endDate });
+    res.status(200).json(trips);
+  } catch (error) {
+    console.error("Error while filtering trips:", error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,8 +5,10 @@ const router = express.Router();
 const emailRoutes = require('./emailRoutes');
 // const userRoutes = require("./userRoutes");
 const authRoutes = require('./authRoutes');
+const tripRoutes = require('./tripRoutes');
 
 router.use('/email', emailRoutes);
 router.use('/auth', authRoutes);
+router.use('/trips', tripRoutes);
 
 module.exports = router;
