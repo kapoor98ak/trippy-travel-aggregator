667775d2c09f668a8a08598ffa7ae79988e654bf
Yash <yashbest005@gmail.com>
2024-07-28 10:36:21 -0300

    update admin routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,34 +2,38 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
-const authMiddleware = async (req, res, next) => {
-  let token;
-  token = req.header('Authorization').replace('Bearer ', '');
+exports.authMiddleware = async (req, res, next) => {
+  let token = req.header('Authorization');
 
   if (!token) {
     return res.status(401).json({ message: 'No token, authorization denied' });
   }
 
+  token = token.replace('Bearer ', '');
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const userId = decoded.user.id;  
+    const userId = decoded.user.id;
     console.log('User ID:', userId);
-   
-
     const user = await User.findById(userId).select('-password');
     if (!user) {
       console.log('No user found with ID:', userId);
       return res.status(404).json({ message: 'User not found' });
     }
     req.user = user;
-
     req.body.user = decoded.id;
     console.log('User:', user);
-
     next();
   } catch (err) {
     res.status(401).json({ message: 'Token is not valid' });
   }
 };
 
-module.exports = authMiddleware;
+exports.verifyAdmin = async (req, res, next) => {
+  await exports.authMiddleware(req, res, async () => {
+    if (req.user.role !== 'admin') {
+      return res.status(403).json({ message: 'Access denied' });
+    }
+    next();
+  });
+};
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,10 +6,12 @@ const emailRoutes = require('./emailRoutes');
 const authRoutes = require('./authRoutes');
 const adminRoutes = require('./adminRoutes')
 const tripRoutes = require('./tripRoutes');
+const adminRoutes = require('./adminRoutes');
 
 router.use('/email', emailRoutes);
 router.use('/auth', authRoutes);
 router.use('/admin', adminRoutes)
 router.use('/trips', tripRoutes);
+router.use('/admin', adminRoutes);
 
 module.exports = router;
