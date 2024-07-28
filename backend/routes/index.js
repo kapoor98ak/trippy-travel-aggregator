678e4c6364be34bc8e773ccc806aca8235b462e8
Yash <yashbest005@gmail.com>
2024-07-28 10:43:25 -0300

    rm duplicate import

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,12 @@ const router = express.Router();
 
 const emailRoutes = require('./emailRoutes');
 const authRoutes = require('./authRoutes');
-const adminRoutes = require('./adminRoutes')
-const tripRoutes = require('./tripRoutes');
 const adminRoutes = require('./adminRoutes');
+const tripRoutes = require('./tripRoutes');
 
 router.use('/email', emailRoutes);
 router.use('/auth', authRoutes);
-router.use('/admin', adminRoutes)
+router.use('/admin', adminRoutes);
 router.use('/trips', tripRoutes);
 router.use('/admin', adminRoutes);
 
