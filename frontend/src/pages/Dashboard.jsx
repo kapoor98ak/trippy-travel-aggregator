66abb4ad44800f6d8f15ab9d767bc04fd15b01ac
Yash <yashbest005@gmail.com>
2024-07-28 10:40:51 -0300

    implement context dashboards for diff users

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import AgentDashboard from "../components/Dashboards/AgentDashboard";
+import TravelerDashboard from "../components/Dashboards/TravelerDashboard";
+import AdminDashboard from "../components/Dashboards/AdminDashboard";
+
+const Dashboard = () => {
+  const auth = JSON.parse(localStorage.getItem("auth"));
+
+  if (!auth) {
+    return <div>Error: User not authenticated</div>;
+  }
+
+  const { role } = auth.user;
+
+  return (
+    <div>
+      {role === "agent" && <AgentDashboard />}
+      {role === "traveler" && <TravelerDashboard />}
+      {role === "admin" && <AdminDashboard />}
+    </div>
+  );
+};
+
+export default Dashboard;
