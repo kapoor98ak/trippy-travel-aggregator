89576a76be3b9bdba5c3093d09f9697426c080d9
Yash <yashbest005@gmail.com>
2024-07-28 10:39:43 -0300

    update routes, create private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-// import "./App.css";
 import { Box } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { ToastContainer } from "react-toastify";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import PrivateRoute from "./components/PrivateRoute";
 import Landing from "./pages/Landing.jsx";
 import FAQ from "./pages/FAQ.jsx";
 import ContactUs from "./pages/ContactUs.jsx";
@@ -14,6 +15,8 @@ import Footer from "./components/Footer.jsx";
 import ForgotPassword from "./components/ForgotPassword.jsx";
 import PasswordReset from "./components/PasswordReset.jsx";
 import TripDetail from "./pages/TripDetail.jsx";
+import UserProfile from "./pages/UserProfile.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 import AdminHomePage from "./pages/AdminHomePage.jsx";
 
 const theme = createTheme({
@@ -38,46 +41,62 @@ const theme = createTheme({
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <ToastContainer
-          position="top-right"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <Box flex flexDirection="column" minHeight="100vh" minWidth="100%">
-          <Header />
-          <Box flexGrow={1} minHeight="100vh" minWidth="100%">
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/contact" element={<ContactUs />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-              <Route
-                exact
-                path="/forgotpassword"
-                element={<ForgotPassword />}
-              />
-              {/* <Route
-              exact
-              path="/reset-password/:token"
-              element={<PasswordReset />}
-            /> */}
-              <Route path="/reset-password" element={<PasswordReset />} />
-              <Route path="/tripdetail" element={<TripDetail />} />
-              <Route path="/admin-home" element={<AdminHomePage></AdminHomePage>} />
-            </Routes>
+      <AuthProvider>
+        <BrowserRouter>
+          <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
+          <Box flex flexDirection="column" minHeight="100vh" minWidth="100%">
+            <Header />
+            <Box flexGrow={1} minHeight="100vh" minWidth="100%">
+              <Routes>
+                <Route path="/" element={<Landing />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/contact" element={<ContactUs />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                  exact
+                  path="/forgotpassword"
+                  element={<ForgotPassword />}
+                />
+                <Route path="/reset-password" element={<PasswordReset />} />
+                <Route path="/tripdetail" element={<TripDetail />} />
+                <Route
+                  path="/profile"
+                  element={
+                    <PrivateRoute>
+                      <UserProfile />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/dashboard"
+                  element={
+                    <PrivateRoute>
+                      <Dashboard />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="/admin-home"
+                  element={<AdminHomePage></AdminHomePage>}
+                />
+              </Routes>
+            </Box>
+            <Footer />
           </Box>
-          <Footer />
-        </Box>
-        <ToastContainer />
-      </BrowserRouter>
+          <ToastContainer />
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
