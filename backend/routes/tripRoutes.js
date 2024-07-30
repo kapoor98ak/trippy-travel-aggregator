f645ae5e0846abc9272701dde959175ef45e85ca
Yash <yashbest005@gmail.com>
2024-07-30 01:35:28 -0300

    add update, create trip routes

--- backend/routes/tripRoutes.js
+++ backend/routes/tripRoutes.js
@@ -1,17 +1,27 @@
-const express = require("express");
+const express = require('express');
 const tripController = require('../controllers/tripController');
 const { authMiddleware } = require('../middleware/authMiddleware');
 const uploadFilesAndConvertToBase64 = require('../middleware/uploadFilesAndConvertToBase64');
 
 const router = express.Router();
 
-router.post('/create', authMiddleware, uploadFilesAndConvertToBase64, tripController.createTrip);
+router.post(
+  '/create',
+  authMiddleware,
+  uploadFilesAndConvertToBase64,
+  tripController.createTrip,
+);
 router.post('/filter', tripController.filterTrips);
 
 router.get('/agent', authMiddleware, tripController.getTripsByAgent);
 
-router.get('/:id', authMiddleware, tripController.getTripById);
+router.get('/:id', tripController.getTripById);
 
-router.put('/:id', authMiddleware, uploadFilesAndConvertToBase64, tripController.editTrip);
+router.put(
+  '/:id',
+  authMiddleware,
+  uploadFilesAndConvertToBase64,
+  tripController.editTrip,
+);
 
 module.exports = router;
