50bd481f714466fa663f1194bd9a7343e77c2efc
abhilatawa <abhisheklatawa1103@gmail.com>
2024-07-30 19:51:05 -0300

    fix: auth state management handled and rendering updated

--- frontend/src/pages/TravelRequestForm.jsx
+++ frontend/src/pages/TravelRequestForm.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { Box, Container, Grid, Paper, Typography, TextField, FormControlLabel, Button, InputAdornment } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import {styled } from '@mui/system';
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { useNavigate } from 'react-router-dom';
 import Checkbox from '@mui/material/Checkbox';
-// import SideNavBar from '../components/SideNavBar';
+
+
 import Copyright from '@mui/icons-material/Copyright';
+import {toast} from "react-toastify";
+import axiosInstance from "../api/Axios.jsx";
+import { AuthContext } from "../context/AuthContext";
+
 
 const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
@@ -20,6 +25,20 @@ function TravelRequestPage() {
     const [info, setInfo] = useState('');
     const [source, setSource] = useState('');
     const [destination, setDestination] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [budget, setBudget] = useState(0);
+    const [itineraryDetails, setItineraryDetails] = useState('');
+    const [numOfTravellers, setNumOfTravellers] = useState('');
+
+
+    const [touched, setTouched] = useState({
+        info: false,
+        source: false,
+        destination: false,
+    });
+
     const [checklist, setChecklist] = useState({
         food: false,
         Accommodation: false,
@@ -27,20 +46,69 @@ function TravelRequestPage() {
         guide: false,
     });
 
-    const navigate = useNavigate();
+    const navigate = useNavigate()
+    const { auth } = useContext(AuthContext);
 
     const handleBackClick = () => {
         navigate('/dashboard');
     };
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        if (info === '' || source === '' || destination === '') {
-            alert('Please fill all required fields');
-            return;
-        } else {
-            navigate('/dashboard');
+        setFormSubmitted(true);
+        if (!info || !source || !destination || !startDate || !endDate || !budget || !numOfTravellers) {
+            toast.error("Please fill all the required fields correctly");
+
         }
+
+       // if (!auth ) {
+        //     toast.error("User ID not found. Please log in again.");
+        //     return;
+        // }
+
+        const travelerId = auth.user._id;
+        const token = auth.token;
+        if (!travelerId) {
+            toast.error("Traveler ID not found. Please log in again.");
+            return;
+        }
+        const requestData = {
+            title: info,
+            source:source,
+            destination:destination,
+            startDate :startDate.toISOString().split('T')[0],
+            endDate :endDate.toISOString().split('T')[0],
+            budget:budget,
+            itineraryDetails:itineraryDetails,
+            numOfTravellers:numOfTravellers,
+            amenities: Object.keys(checklist).filter(key => checklist[key]),
+
+        };
+        console.log(token);
+        try {
+            const response = await axiosInstance.post('travelrequests/create-request',
+                JSON.stringify(requestData),
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${token}`,
+                    },
+                }
+            );
+
+
+            const result =  response;
+            if (response) {
+                alert('Request created successfully');
+                navigate("/display-requests")
+            } else {
+                toast.error('Error: ' + result.error);
+            }
+        } catch (error) {
+            console.error('Error creating request:', error);
+            toast.error('Error creating request');
+        }
+
     };
 
     const handleChange = (event) => {
@@ -50,6 +118,10 @@ function TravelRequestPage() {
             [name]: checked,
         }));
     };
+    const handleBlur = (field) => {
+        setTouched(prev => ({ ...prev, [field]: true }));
+    };
+
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -76,40 +148,43 @@ function TravelRequestPage() {
                                     <Grid container spacing={2} sx={{ mt: 1 }}>
                                         <Grid item md={12} xs={12}>
                                             <TextField
-                                                required
                                                 fullWidth
                                                 label="Enter the Trip Title"
                                                 value={info}
                                                 onChange={(e) => setInfo(e.target.value)}
-                                                error={info === ''}
-                                                helperText={info === '' ? 'Please enter the trip name' : ''}
+                                                onBlur={() => handleBlur('info')}
+                                                error={(formSubmitted || touched.info) && info === ''}
+                                                helperText={(formSubmitted || touched.info) && info === '' ? 'Please enter the trip name' : ''}
                                             />
                                         </Grid>
                                         <Grid item md={6} xs={12}>
                                             <TextField
-                                                required
                                                 fullWidth
                                                 label="Enter Source"
                                                 value={source}
                                                 onChange={(e) => setSource(e.target.value)}
-                                                error={!source}
-                                                helperText={!source ? 'Please enter the source' : ''}
+                                                onBlur={() => handleBlur('source')}
+                                                error={(formSubmitted || touched.source) && !source}
+                                                helperText={(formSubmitted || touched.source) && !source ? 'Please enter the source' : ''}
                                             />
+
                                         </Grid>
                                         <Grid item md={6} xs={12}>
                                             <TextField
-                                                required
                                                 fullWidth
                                                 label="Enter Destination"
                                                 value={destination}
                                                 onChange={(e) => setDestination(e.target.value)}
-                                                error={!destination}
-                                                helperText={!destination ? 'Please enter the destination' : ''}
+                                                onBlur={() => handleBlur('destination')}
+                                                error={(formSubmitted || touched.destination) && !destination}
+                                                helperText={(formSubmitted || touched.destination) && !destination ? 'Please enter the destination' : ''}
                                             />
                                         </Grid>
                                         <Grid item md={6} xs={12}>
                                             <LocalizationProvider dateAdapter={AdapterDayjs}>
                                                 <DatePicker
+                                                    //value={startDate}
+                                                    onChange={(newValue) => setStartDate(newValue)}
                                                     label="Enter Start Date"
                                                     renderInput={(props) => <TextField {...props} required fullWidth />}
                                                 />
@@ -118,6 +193,7 @@ function TravelRequestPage() {
                                         <Grid item md={6} xs={12}>
                                             <LocalizationProvider dateAdapter={AdapterDayjs}>
                                                 <DatePicker
+                                                    onChange={(newValue) => setEndDate(newValue)}
                                                     label="Enter End Date"
                                                     renderInput={(props) => <TextField {...props} required fullWidth />}
                                                 />
@@ -127,7 +203,8 @@ function TravelRequestPage() {
                                             <TextField
                                                 type="number"
                                                 fullWidth
-                                                label="Enter Max Budget"
+                                                label="Enter of Travellers"
+                                                onChange={(e) => setNumOfTravellers(parseInt(e.target.value))}
                                                 InputProps={{
                                                     startAdornment: <InputAdornment position="start">$</InputAdornment>,
                                                 }}
@@ -138,6 +215,7 @@ function TravelRequestPage() {
                                                 multiline
                                                 fullWidth
                                                 label="Itenary Details"
+                                                onChange={(e) => setItineraryDetails(e.target.value)}
                                                 // Define isEditable properly or remove if not needed
                                                 InputProps={{
                                                     readOnly: false,
@@ -146,9 +224,10 @@ function TravelRequestPage() {
                                         </Grid>
                                         <Grid item md={6} xs={12}>
                                             <TextField
-                                                multiline
                                                 fullWidth
-                                                label="No. of Travellers"
+                                                label="Enter Max Budget"
+                                                type="number"
+                                                onChange={(e) => setBudget(parseInt(e.target.value))}
                                                 InputProps={{
                                                     readOnly: false,
                                                 }}
@@ -215,25 +294,6 @@ function TravelRequestPage() {
                         </Grid>
                     </Grid>
                 </Container>
-                {/* <Container component="main" sx={{ mt: 8, mb: 19 }} maxWidth="sm"></Container> */}
-                {/* <Box
-                    component="footer"
-                    maxWidth="100"
-                    sx={{
-                        display: 'flex',
-                        minHeight: '20',
-                        py: 3,
-                        px: 2,
-                        mt: 'auto',
-                        backgroundColor: 'black',
-                        color: 'white',
-                    }}
-                >
-                    <Container maxWidth="lg">
-                        <Typography variant="body1">My sticky footer can be found here.</Typography>
-                        <Copyright />
-                    </Container>
-                </Box> */}
             </Box>
         </Box>
     );
