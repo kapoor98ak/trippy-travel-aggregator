6943d3cf5f5359e16ea25fc4e6d097cd77bedde4
PARTHRAJ PANCHAL <pr829133@dal.ca>
2024-08-07 17:57:40 -0300

    Implementation | Admin email configrued in contact us page

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -29,6 +29,7 @@ const ContactUs = () => {
 
   const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
   const nameRegex = /^[A-Za-z]+$/;
+  const adminEmail = "kapoor28204@gmail.com"
 
   function handleSubmit() {
     if (areAllInputsValid()) {
@@ -41,7 +42,7 @@ const ContactUs = () => {
   async function sendEmailToAdmin() {
     try {
       const response = await axiosInstance.post('/email/send', {
-        to: emailRef.current.value,
+        to: adminEmail,
         subject: `Customer contact request | ${firstNameRef.current.value}`,
         text: `${firstNameRef.current.value} ${lastNameRef.current.value} want to connect to you about the trippy. Their email: ${emailRef.current.value}. Message : ${messageRef.current.value} . Happy conversation !`
       })
